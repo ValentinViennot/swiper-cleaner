b1bd687e6cae44364c08ff00225a69d714f387f3
xsbfh <z6MkpzZDoFM7tPZPXqvaVCtMFxKJQAxghVRJZxsf8s8SqBvm@dev.smashchats.com>
2025-01-23 23:12:37 +0000

    configure for build

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,12 +1,13 @@
-echo -e "\nStarting post creat command script..."
+echo -e "\nStarting post create command script..."
 echo "Dev machine:"
 uname -a
-echo -e "\nInstalling expo boiler plate..."
-npm install --save-dev -y create-expo-app@2.1.1
-echo -e "\nInstalling watchman...\n"
-sudo apt-get update
-sudo apt-get install -y watchman
-watchman version
+
+# echo -e "\nInstalling expo boiler plate..."
+# npm install --save-dev -y create-expo-app@2.1.1
+# echo -e "\nInstalling watchman...\n"
+# sudo apt-get update
+# sudo apt-get install -y watchman
+# watchman version
 
 echo -e "\n*******************************"
 echo -e "\nDev container ready!".
